b20a97dd0e6aa294d94ad61294851f0b84d3bbd2
piapip <thovinh9997@gmail.com>
2021-01-05 21:01:15 +0700

    Finish uploading locally

---
+++ client/src/components/views/Chatroom/Section/Test.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Button } from 'antd'
+import axios from 'axios'
+
+import { BACKEND_URL } from '../../../Config'
+
+export default function Test(props) {
+
+  const data = props ? props.audio : null
+
+  // const submit = (e) => {
+  //   e.preventDefault()
+  //   fetch('/')
+  //   console.log("Submit!")
+  // }
+
+  const uploadAudio = async (e) => {
+    e.preventDefault()
+
+    // create data
+    let formdata = new FormData()
+    formdata.append('soundBlob', data.blob, 'test.wav')
+     
+    const requestConfig = {     
+      headers: new Headers({
+        enctype: "multipart/form-data"
+      })
+    }
+    
+    try {
+      await axios.post(
+        `${BACKEND_URL}/api/upload/file`,
+        formdata,
+        requestConfig,
+      ).then(res => {
+          console.log(res)
+      })
+  } catch(error){
+      alert(error)
+  }
+
+  }
+
+  const insertButton = data !== null ? (
+    <Button onClick={uploadAudio}>Sent</Button>
+  ) : ""
+
+  return (
+    <>
+      {insertButton}
+    </>    
+  )
+}
