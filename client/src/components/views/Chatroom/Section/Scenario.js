d215a7c5afb66dde024225418f065d57613533ec
Công Phùng Thành <20160500@student.hust.edu.vn>
2021-01-16 19:20:19 +0700

    add servant screen

--- client/src/components/views/Chatroom/Section/Scenario.js
+++ client/src/components/views/Chatroom/Section/Scenario.js
@@ -15,8 +15,8 @@ export default function Scenario() {
   }
   return (
       <div style={{display:"flex",flexDirection:"column",justifyContent:"space-between",alignItems:"stretch"}}>
-        <Row style={{borderLeft:"1px solid",height:"100%"}}>
-          <h3 style={{fontWeight:'bold',fontSize:'18px'}}>Kịch bản hội thoại</h3>
+        <Row style={{borderLeft:"1px solid",height:"100%",borderColor:"white",backgroundColor:"white"}}>
+          <h3 style={{fontWeight:'bold',fontSize:'18px',textAlign: "center"}}>Kịch bản hội thoại</h3>
           <Col span={24} style={{fontSize:"15px",marginTop:"auto"}}>Bạn muốn bật lò vi sóng trong phòng ăn ở tầng 1. Hãy nói yêu cầu trên bằng tiếng
             Việt ( có thể bằng 1 hoặc nhiều lần nói).</Col>
           <Col span={24}>
