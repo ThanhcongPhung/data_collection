de0d7ef8165e0fbd317de1bb326e18bea5832967
piapip <thovinh9997@gmail.com>
2021-02-01 18:37:11 +0700

    Add timer

--- client/src/components/views/LandingPage/Section/ReadyButton.js
+++ client/src/components/views/LandingPage/Section/ReadyButton.js
@@ -1,13 +1,51 @@
-import React from 'react'
-import { Button } from 'antd'
+import React, { useState, useRef } from 'react'
+import { Button, Row, Col } from 'antd'
 
 export default function ReadyButton(props) {
+  
+  const [ timer, setTimer ] = useState(0)
+  const increment = useRef(null)
+
+  const ready = () => {
+    props.readySignal()
+    // start counting
+    increment.current = setInterval(() => {
+      setTimer((timer) => timer + 1)
+    }, 1000)
+  }
+
+  const cancelReady = () => {
+    props.cancelReadySignal()
+    // stop counting
+    clearInterval(increment.current)
+    setTimer(0)
+  }
+
+  const timeConverter = (seconds) => {
+    const format = val => `0${Math.floor(val)}`.slice(-2)
+    const hours = seconds / 3600
+    const minutes = (seconds % 3600) / 60
+
+    return [hours, minutes, seconds % 60].map(format).join(':')
+  }
+
   return (
-    (!props.readyStatus ? (
-      <Button onClick={props.readySignal}>Sẵn sàng</Button>
-    ): (
-      <Button onClick={props.cancelReadySignal}>Dừng tìm kiếm</Button>
-    ))
-    
+    <>
+      <Row>
+        <Col style={{textAlign: "center"}}>
+          {!props.readyStatus ? (
+            <Button onClick={ready}>Sẵn sàng</Button>
+          ): (
+            <Button onClick={cancelReady}>Dừng tìm kiếm</Button>
+          )}
+        </Col>
+      </Row>
+      
+      <Row>
+        <Col>
+          Đang tìm bạn: {timeConverter(timer)}
+        </Col>
+      </Row>    
+    </>
   )
 }
