e4229083479a56f2292b0ea2c08408055981941f
Công Phùng Thành <20160500@student.hust.edu.vn>
2021-02-06 11:05:00 +0700

    add aws config

--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ const path = require("path");
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-
+const AWS= require('aws-sdk');
 const config = require("./config/key");
-
+const fs = require('fs');
 const mongoose = require("mongoose");
 const connect = mongoose.connect(config.mongoURI,
     {
@@ -16,8 +16,49 @@ const connect = mongoose.connect(config.mongoURI,
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
+const ID='ASIA5H5AVHPSSIISKK6P';
+const SECRET = 'r96ZQyAZqf/2dIuDRvW6n/iZm7G3AiaFhXOYKjrF';
+const SESSION = 'FwoGZXIvYXdzEEoaDDih5VuY7RenJ3mCkSLMAdPrCpXreIQ9I2UuES2YkXnrQIqJvdxxCBpPYp4az/i/yMflbb/hEyccgVchRueJvnw1RTiBIfdPKqoKCtRfdaeQW28SODjbxf4NRIKmsxc3UqlIGZWZfAfijRpFoGHWTpUfl4pEw53uQU92BpV1vMYsCJPv1KUEwd/+b0Jlx9yOxv7d335ssXXYgfND4tJTYaZj6VF2SNaEB4drg87NXEOHqH73BhPtBnZfs/paaM2ZKB/L0IDJhjF4YYfEYYQA5dnWVQaQMkoaj1wTGyi47PWABjItd2uGN5kYq7DoE5rVvUQ5fOz6BkhE7Xr7f7rxlBjoaLs9fvy/g3KEazyYV3Qx'
+const BUCKET_NAME = 'data-collection-20202';
 
-require("./models/Message")
+const s3 = new AWS.S3({
+  accessKeyId: ID,
+  secretAccessKey: SECRET,
+  sessionToken: SESSION
+})
+// const params = {
+//   Bucket: BUCKET_NAME,
+//   CreateBucketConfiguration: {
+//     // Set your region here
+//     LocationConstraint: "eu-west-1"
+//   }
+// };
+
+// s3.createBucket(params, function(err, data) {
+//   if (err) console.log(err, err.stack);
+//   else console.log('Bucket Created Successfully', data.Location);
+// });
+const fileName= 'server/public/audio/2021/02/01/1kbtFDkDQexTnGw6.wav'
+const uploadFile = (fileName) => {
+  // Read content from the file
+  const fileContent = fs.readFileSync(fileName);
+
+  // Setting up S3 upload parameters
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: 'cat.wav', // File name you want to save as in S3
+    Body: fileContent
+  };
+
+  // Uploading files to the bucket
+  s3.upload(params, function(err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log(`File uploaded successfully. ${data.Location}`);
+  });
+};
+ require("./models/Message")
 
 app.use(cors())
 
