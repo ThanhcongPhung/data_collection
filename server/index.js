e7097dfc5777d11bcc1da55e23c6e1b7bef26c18
congpt <20160500@student.hust.edu.vn>
2021-04-29 11:24:43 +0700

    remove product index.html

--- server/index.js
+++ server/index.js
@@ -60,22 +60,22 @@ app.use('/api/validate', require('./routes/validate'));
 
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
-app.use('/uploads', express.static('uploads'));
+// app.use('/uploads', express.static('uploads'));
 
-app.use(express.static(__dirname))
+// app.use(express.static(__dirname))
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-
-  // Set static folder   
-  // All the javascript and css files will be read and served from this folder
-  app.use(express.static("client/build"));
-
-  // index.html for all page routes    html or routing and naviagtion
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
-}
+// // Serve static assets if in production
+// if (process.env.NODE_ENV === "production") {
+//
+//   // Set static folder
+//   // All the javascript and css files will be read and served from this folder
+//   app.use(express.static("client/build"));
+//
+//   // index.html for all page routes    html or routing and naviagtion
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+//   });
+// }
 
 const port = process.env.PORT || 4000
 
